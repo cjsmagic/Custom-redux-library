8c0ea793cc27acc45af9ba6fa8fee8f5dec2d662
cjsmagic <clarencemerchant1995@gmail.com>
2023-08-07 05:38:08 +0530

    3 files modified, 1 added

--- src/react-redux.js
+++ src/react-redux.js
@@ -1,33 +1,36 @@
 import React, { useState, useEffect, createContext, useContext } from 'react';
 
-
 const Context = createContext();
 
-function Provider({store,children}){
-  return <Context.Provider value={store}>{children}</Context.Provider>
-
+function Provider({ store, children }) {
+  return <Context.Provider value={store}>{children}</Context.Provider>;
 }
 
 function connect(Component, mapPropsToState, dispatchConsumers) {
-
-  return function wrapperComponent(defaultProps={}){
+  return function wrapperComponent(defaultProps = {}) {
     const store = useContext(Context);
-    const [mappedProps,setMappedProps] = useState(()=>({...mapPropsToState(store.getValue()),...dispatchConsumers(store.dispatch)}))
 
-    useEffect(()=>{
-      const unsubscribe = store.subscribe(()=>{
-        setMappedProps({...mapPropsToState(store.getValue()),...dispatchConsumers(store.dispatch)})
-      })
+    const getProps = () => {
+      return {
+        ...mapPropsToState(store.getValue()),
+        ...dispatchConsumers(store.dispatch),
+      };
+    };
 
-      return ()=>{
+    const [mappedProps, setMappedProps] = useState(() => getProps());
+
+    useEffect(() => {
+      const unsubscribe = store.subscribe(() => {
+        setMappedProps(getProps());
+      });
+
+      return () => {
         unsubscribe();
-      }
-    },[])
+      };
+    }, []);
 
-    return <Component {...defaultProps,mappedProps}/>
-  }
-
+    return <Component {...{ ...defaultProps, ...mappedProps }} />;
+  };
 }
 
-
-export { Provider,connect };
+export { Provider, connect };
---
+++ src/without-react-redux.js
@@ -0,0 +1,76 @@
+// import React, { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './App';
+// import store from './store';
+// import { Provider } from './react-redux';
+
+// const initialState = {
+//   count: 0,
+// };
+
+// function myReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case 'add':
+//       return { ...state, count: state.count + 1 };
+
+//     case 'sub':
+//       return { ...state, count: state.count - 1 };
+
+//     default:
+//       return state;
+//   }
+// }
+
+// const store = createStore(myReducer);
+
+// store.subscribe(() => {
+//   console.log(store.getValue());
+// });
+
+// console.log(store.getValue());
+
+// store.dispatch({ type: 'add' });
+
+// console.log(store.getValue());
+
+// store.dispatch({ type: 'sub' });
+
+// console.log(store.getValue());
+
+// store.dispatch({ type: 'add' });
+
+// export default function App() {
+//   const [value, setValue] = useState(0);
+
+//   const store = useRef(null);
+
+//   useEffect(() => {
+//     store.current = createStore(myReducer);
+
+//     const unsubscribe = store.current.subscribe(() => {
+//       const { count } = store.current.getValue();
+//       console.log({ count });
+//       setValue(count);
+
+//       if (count === 11) {
+//         unsubscribe();
+//       }
+//     });
+//   }, []);
+
+//   const add = () => {
+//     store.current.dispatch({ type: 'add' });
+//   };
+
+//   const subtract = () => {
+//     store.current.dispatch({ type: 'sub' });
+//   };
+
+//   return (
+//     <div>
+//       <h1>Count: {value}</h1>
+//       <button onClick={add}>Add</button>
+//       <button onClick={subtract}>Subtract</button>
+//     </div>
+//   );
+// }
